bcc470b1ea01816ce1eaf07493d4af589017c428
Christian Gati <christian.gati@gmail.com>
2016-01-08 12:16:55 -0500

    remove unnecessary scope

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let arguments = env::args().collect::<Vec<_>>();
     
     match arguments.len() {
-        1 => { loop { println!("y") } },
-        _ => { loop { println!("{}", arguments[1]) } },
+        1 => loop { println!("y") },
+        _ => loop { println!("{}", arguments[1]) },
     }
 }
